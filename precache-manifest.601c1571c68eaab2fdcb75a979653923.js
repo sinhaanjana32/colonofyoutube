a096b83b8a3c5dada8e404edfbb05e04230435da
Anjana <sinhaanjana32@gmail.com>
2020-06-30 14:50:32 +0300

    Updates

---
+++ precache-manifest.601c1571c68eaab2fdcb75a979653923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc71dbf4ac6c02ccbc407225af06a216",
+    "url": "/colonofyoutube/index.html"
+  },
+  {
+    "revision": "3962288bc2a2c2a80156",
+    "url": "/colonofyoutube/static/js/2.758ecbc9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/colonofyoutube/static/js/2.758ecbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b61c8e88a14f984a30",
+    "url": "/colonofyoutube/static/js/main.a84f1482.chunk.js"
+  },
+  {
+    "revision": "a102326d44586ccd0b08",
+    "url": "/colonofyoutube/static/js/runtime-main.bb3677d6.js"
+  }
+]);
